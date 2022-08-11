6a04335e354a9c23aafa70bb65f918f2b5fa70ed
yesg98 <61418190+yesg98@users.noreply.github.com>
2022-08-11 14:59:35 +0900

    Update achieve_progress_lev.js
    DB내에서 불러오기 메소드 구현,
    새로운 유저 DB생성 메소드 구현,
    모델 숫자 초기값 0으로 설정

--- src/models/achieve_progress_lev.js
+++ src/models/achieve_progress_lev.js
@@ -5,29 +5,53 @@ const crypto = require('crypto');
 
 const Achieve_progress_lev = new Schema({ // 개인 진행도 저장용 DB
     Id: String, // 사용자 ID
-    vol_time: Number, // 봉사시간
-    Progressed_achieve_num: Number, // 업적 달성 갯수
-    Done_num: Number // 기부액
+    vol_time: {type: Number, default: 0}, // 봉사시간
+    Progressed_achieve_num: {type: Number, default: 0}, // 업적 달성 갯수
+    Done_num: {type: Number, default: 0} // 기부액
 });
 
-Achieve_progress_lev.static.findVolTime = function(id){
+Achieve_progress_lev.static.UserRegist = function(id){ // 새로운 유저DB생성
+    const user = new this({
+        Id: id
+    });
+    return user.save();
+};
+
+Achieve_progress_lev.static.findVolTime = function(){
     // ID입력해 봉사시간 불러오기
     var data = this.findOne({'Id':id}).exec();
     return data.vol_time;
-}
+};
+
+Achieve_progress_lev.static.VolTimeUpdate = function({id, VolTime}){
+    // 봉사시간 업데이트
+    var User = this.findOne({'Id':id}).exec();
+    User.vol_time = VolTime;
+};
 
 Achieve_progress_lev.static.findDoneNum = function(id){
     // ID입력해 기부액 불러오기
     var data = this.findOne({'Id':id}).exec();
     return data.Done_num;
-}
+};
+Achieve_progress_lev.static.DoneUpdate = function({id, DoneNum}){
+    // 기부액 업데이트
+    var User = this.findOne({'Id':id}).exec();
+    User.Done_num = DoneNum
+};
 Achieve_progress_lev.static.findAchieveNum = function(id){
+    // ID입력해 업적 달성갯수 불러오기
     var data = this.findOne({'Id':id}).exec();
     return data.Progressed_achieve_num;
-}
+};
+Achieve_progress_lev.static.AchieveCount = function(id){
+    // ID입력해 업적 달성갯수 올리기
+    var User = this.findOne({'Id':id}).exec();
+    User.Progressed_achieve_num++;
+};
 Achieve_progress_lev.static.findAllProgress = function(id){
     // ID 입력해 전체 진행도 불러오기
     return this.findOne({'Id':id}).exec();
-}
+};
 
 module.exports = mongoose.model('Achieve_progress_lev', Achieve_progress_lev);
