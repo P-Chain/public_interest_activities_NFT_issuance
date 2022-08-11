f095c7e8dec9155904eed94e9c5dcf3ec3fa08ee
yesg98 <61418190+yesg98@users.noreply.github.com>
2022-08-11 15:13:18 +0900

    Update progessed_achieve.js
    새로운 유저 추가 기능추가,
    사용자 달성 업적 추가기능 추가

--- src/models/progessed_achieve.js
+++ src/models/progessed_achieve.js
@@ -8,9 +8,23 @@ const Progressed_achieve = new Schema({ // 업적 달성여부 DB
     Prog_achieve: [{id_ach: Number, name_ach: String, date_ach: Date}] // 달성업적 저장용 배열(업적id, 업적이름, 달성날짜 저장)
 });
 
+Progressed_achieve.static.AddUser = function(id){
+    //새로운 유저 추가
+    const User = new this({
+        Id:id
+    });
+};
+
 Progressed_achieve.static.findAchieveArray = function(id){
     // 사용자 ID입력받아 업적달성 배열 불러오기
-    return this.findOne({'Id':id}).exec();
-}
+    var User = this.findOne({Id:id}).exec();
+    return User.Prog_achieve;
+};
+Progressed_achieve.static.AddProgAchieve = function({id, IdAch, NameAch, DateAch}){
+    // 사용자 달성 업적 추가
+    var User = this.findOne({Id:id}).exec();
+    var Prog = User.Prog_achieve;
+    Prog.push({id_ach:Idach, name_ach:NameAch, date_ach:DateAch});
+};
 
 module.exports = mongoose.model('Progressed_achieve', Progressed_achieve);
