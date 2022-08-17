5dcec6199c031b55e6d5b53c829704bb5e9bcea7
yesg98 <61418190+yesg98@users.noreply.github.com>
2022-08-17 18:09:22 +0900

    db테스트 진행
    main.mjs로 변경, db test 추가
    db 메소드 배열 push 추가

--- src/models/progressed_achieve.js
+++ src/models/progressed_achieve.js
@@ -5,7 +5,7 @@ const crypto = require('crypto');
 
 const Progressed_achieve = new Schema({ // 업적 달성여부 DB
     Id: String, // 사용자 ID
-    Prog_achieve: [{id_ach: Number, name_ach: String, date_ach: Date}] // 달성업적 저장용 배열(업적id, 업적이름, 달성날짜 저장)
+    Prog_achieve: {type:[{id_ach: Number, name_ach: String, date_ach: Date}], default: []} // 달성업적 저장용 배열(업적id, 업적이름, 달성날짜 저장)
 });
 
 Progressed_achieve.statics.AddUser = function(id){
@@ -18,14 +18,12 @@ Progressed_achieve.statics.AddUser = function(id){
 
 Progressed_achieve.statics.findAchieveArray = function(id){
     // 사용자 ID입력받아 업적달성 배열 불러오기
-    var User = this.findOne({Id:id}).exec();
-    return User.Prog_achieve;
+    return this.findOne({Id:id}).exec();
 };
-Progressed_achieve.statics.AddProgAchieve = function({id, IdAch, NameAch, DateAch}){
+Progressed_achieve.statics.AddProgAchieve = function(id, IdAch, NameAch, DateAch){
     // 사용자 달성 업적 추가
-    var User = this.findOne({Id:id}).exec();
-    var Prog = User.Prog_achieve;
-    Prog.push({id_ach:Idach, name_ach:NameAch, date_ach:DateAch});
+    this.updateOne({Id:id},{$push: {Prog_achieve: {id_ach:IdAch, name_ach:NameAch, date_ach:DateAch}}}).exec();
+    
 };
 
 module.exports = mongoose.model('Progressed_achieve', Progressed_achieve);
