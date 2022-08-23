351b0b5a41799b71ba513ceac1e1c5293b537d42
MadCom96 <70102600+MadCom96@users.noreply.github.com>
2022-08-23 17:31:55 +0900

    modified vms controller. not completed yet.

--- src/vms_ins/vms_ins.controller.js
+++ src/vms_ins/vms_ins.controller.js
@@ -1,12 +1,14 @@
 const Joi = require('joi');
 const Request = require('request');
 
-const baseUrl = 'https://www.vms.or.kr/auth/authenticationView.do';
+// const baseUrl = 'https://www.vms.or.kr/auth/authenticationView.do';
+const baseUrl = 'https://www.vms.or.kr/auth/authentication.do';
 
 // 작동하지 않습니다. (수정해야합니다...)
 // 실제 있는 인증번호인지 검사
 exports.isValidNum = async (ctx) => {
-    process.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+    // process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+
     const schema = Joi.object().keys({
         certifyno1: Joi.string().min(8).max(8).required(),
         certifyno2: Joi.string().min(4).max(4).required(),
@@ -26,6 +28,7 @@ exports.isValidNum = async (ctx) => {
         method: 'POST',
         form: ctx.request.body
     };
+
     Request.post(option, function(error, response, body) {
         console.log('error : ', error)
         console.log('statusCode :', response && response.statusCode); // Print the response status code if a response was received
