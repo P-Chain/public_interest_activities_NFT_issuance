4ddb488108a1375054f8c24a55ba6b9e3b0bc163
MadCom96 <70102600+MadCom96@users.noreply.github.com>
2022-09-16 14:42:19 +0900

    sending ether done
    (중요)need more work about address and private key.

--- src/lib/metamask.js
+++ src/lib/metamask.js
@@ -14,34 +14,34 @@ var node_host = `https://ropsten.infura.io/v3/${infura_token}`;
 
 var web3 = new Web3(node_host); // 소문자 web3
 
-const send_account = "0xF2050Aa76D860167e35D4b34CDB4Ccc841d215F6"; // sender account
-const receive_account = "0x8A2fd42c47C41888FaA695cd3272Ca0B048a2eA7"; // receiver account
+const send_account = "0xC14656C3e35e0f53095021958e74e909D14C413C"; // Account1 account
+const receive_account = "0x39b4a18A7435eF1ED5ecbb8bdc28970e62FF524e"; // transactionBody account
 
-const privateKeyBuffer = Buffer.from(private_key, "hex");
+exports.getTC = async (ctx) => {
+    var bodyString = '';
+    web3.eth.getTransactionCount(send_account, (err, txCount) => { // (1)
+        const txObject = {
+            nonce: web3.utils.toHex(txCount),
+            gasLimit: web3.utils.toHex(1000000),
+            gasPrice: web3.utils.toHex(web3.utils.toWei("10", "gwei")),
+            to: receive_account,
+            value: "0x2C68AF0BB140000"
+        };
+        const tx = new Tx(txObject);
+        tx.sign(privateKeyBuffer);
 
-web3.eth.getTransactionCount(send_account, (err, txCount) => { // (1)
-    const txObject = {
-        nonce: web3.utils.toHex(txCount),
-        gasLimit: web3.utils.toHex(1000000),
-        gasPrice: web3.utils.toHex(web3.utils.toWei("10", "gwei")),
-        to: receive_account,
-        value: "0x2C68AF0BB140000"
-    };
-
-    const tx = new Tx(txObject);
-    tx.sign(privateKeyBuffer);
-
-    const serializedTx = tx.serialize();
-    const raw = "0x" + serializedTx.toString("hex");
-
-    web3.eth
-        .sendSignedTransaction(raw) // (2)
-        .once("transactionHash", hash => {
-            console.info("transactionHash", "https://ropsten.etherscan.io/tx/" + hash);
-            // tx가 pending 되는 즉시 etherscan에서 tx 진행상태를 보여주는 링크를 제공해줍니다.
-        })
-        .once("receipt", receipt => {
-            console.info("receipt", receipt); // 터미널에 receipt 출력
-        })
-        .on("error", console.error);
-});
+        const serializedTx = tx.serialize();
+        const raw = "0x" + serializedTx.toString("hex");
+        web3.eth
+            .sendSignedTransaction(raw) // (2)
+            .once("transactionHash", hash => {
+                console.info("transactionHash", "https://ropsten.etherscan.io/tx/" + hash);
+                // tx가 pending 되는 즉시 etherscan에서 tx 진행상태를 보여주는 링크를 제공해줍니다.
+            })
+            .once("receipt", receipt => {
+                console.info("receipt", receipt); // 터미널에 receipt 출력
+            })
+            .on("error", console.error);
+    });
+    ctx.body = bodyString;
+};
